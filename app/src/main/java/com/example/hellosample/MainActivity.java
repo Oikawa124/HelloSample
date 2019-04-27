8a1e900ddd885ffdf8cd56043a3d1264c2b9f810
devel <oiku1515@gmail.com>
2019-04-27 20:12:13 +0900

    課題完了

--- app/src/main/java/com/example/hellosample/MainActivity.java
+++ app/src/main/java/com/example/hellosample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hellosample;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btClick = findViewById(R.id.button);
+        HelloListener listener = new HelloListener();
+        btClick.setOnClickListener(listener);
+
+        Button btClear = findViewById(R.id.button2);
+        btClear.setOnClickListener(listener);
+
+    }
+
+    private class HelloListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view){
+            EditText input = findViewById(R.id.editText2);
+            TextView output = findViewById(R.id.textView2);
+
+            int id = view.getId();
+            System.out.println(id);
+
+            switch (id) {
+
+                case R.id.button:
+                    String inputStr = input.getText().toString();
+                    output.setText(inputStr + "さん、こんばんは");
+                    break;
+
+                case R.id.button2:
+                    input.setText("");
+                    output.setText("");
+                    break;
+            }
+        }
     }
 }
